480ec71c2cbdb3bc6976f5885401b461d53ecaea
Dimitar765 <112801140+Dimitar765@users.noreply.github.com>
2023-03-04 19:35:55 +0100

    class_03_homework

---
+++ nodeJs/class_03_homework/index.js
@@ -0,0 +1,41 @@
+import { EventEmitter } from "events";
+import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
+// import { stringify } from "querystring";
+
+// import path from "path";
+
+const pathToLog = ".//db/greeting_log.txt";
+const pathToJson = ".//db/students.json";
+
+function CreateStudent(id, fullName, email, password) {
+  this.id = uuidv4();
+  this.name = fullName;
+  this.email = email;
+  this.password = password;
+}
+
+const newStudent = new CreateStudent(
+  "id",
+  "Petar Pejkovski",
+  "petar@pejkovski.com",
+  "qwerty"
+);
+// const anotherStudent = new CreateStudent(
+//   "id",
+//   "Jovan Jovanov",
+//   "jovan@jovanov.com",
+//   "qwerty"
+// );
+
+// console.log(newStudent);
+// console.log(anotherStudent);
+
+const emitter = new EventEmitter();
+emitter.on("greet", () => {
+  console.log(`Welcome ${newStudent.name}`);
+  fs.appendFileSync(pathToLog, `\n${newStudent.name}`);
+  fs.appendFileSync(pathToJson, JSON.stringify(newStudent));
+});
+
+emitter.emit("greet");
